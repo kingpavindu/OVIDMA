2ee29b0c1b2d6742c25a4fd6dcc5aecc9196739e
avi-verma-web <techking2008515@gmail.com>
2024-06-17 11:10:05 +0530

    mobile-nav

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/main/header.css'
-import '../styles/utils.css'
 import '../styles/main/facilities.css'
 import '../styles/main/faq.css'
+import '../styles/main/mobile-nav.css'
+import '../styles/utils.css'
 
 
 
   
   
-  
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
